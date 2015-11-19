3fd5f3beedeae758e8a12c9d1efa58e8966b2a7c
sandun <wakschamara@gmail.com>
2015-11-19 11:54:36 +0530

    admin zone added

---
+++ administrator/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to ElevenOne Foods</title>
+	<link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/main.css" media="screen" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style-portfolio.css">
+    <link rel="stylesheet" href="../css/picto-foundry-food.css" />
+    <link rel="stylesheet" href="../css/jquery-ui.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/font-awesome.min.css" rel="stylesheet">
+    <link rel="icon" href="../favicon-1.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/javascript" href="../js/bootstrap.min.js">
+</head>
+<body background="../images/landing_bg.jpg">
+	<div id="header">
+		<img src="../images/logo.png" width="500px">
+	</div>
+	<div id="content">
+		<form class="box login" style="margin-top:-10%;">
+			<fieldset class="boxBody">
+				<h2>Administrator Login</h2>
+				<hr>
+			  <label>Username</label>
+			  <input type="text" tabindex="1" placeholder="Admin" required>
+			  <label><a href="#" class="rLink" tabindex="5">Forget your password?</a>Password</label>
+			  <input type="password" tabindex="2" required>
+			</fieldset>
+			<footer>
+			  <label><input type="checkbox" tabindex="3">Keep me logged in</label>
+			  <input type="submit" class="text-center form-btn form-btn" value="Login" style="width: 100px; float: right;" tabindex="4">
+			</footer>
+		</form>
+	</div>
+	<div id="footer">
+	<div>
+		<ul>
+            <li><a class="icon-twitter color_animation" href="#" target="_blank"></a></li>
+            <li><a class="icon-github color_animation" href="#" target="_blank"></a></li>
+            <li><a class="icon-linkedin color_animation" href="#" target="_blank"></a></li>
+        	<li><a class="icon-mail color_animation" href="#"></a></li>
+        </ul>
+	</div>
+		<p>© 2015 ElevenOne Foods, (PVT)LTD. All rights reserved.</p>
+	</div>
+
+</body>
+</html>
